04aa7f33247c9f1f5035e87978c36a2d9b9523f4
Roadagain <ringoh72@gmail.com>
2017-04-03 18:11:10 +0900

    typescriptのタスクを定義

--- gulpclass.ts
+++ gulpclass.ts
@@ -3,6 +3,7 @@
 import {Gulpclass, Task} from "gulpclass/Decorators";
 
 import * as gulp from "gulp";
+import * as tsc from "gulp-typescript";
 
 @Gulpclass()
 export class Gulpfile {
@@ -10,6 +11,13 @@ export class Gulpfile {
   @Task()
   default() {
     return ["test"];
+  }
+
+  @Task()
+  typescript() {
+    return gulp.src(tsconfig.include)
+      .pipe(tsc(tsconfig.compilerOptions))
+      .pipe(gulp.dest(tsconfig.compilerOptions.outDir));
   }
 
   @Task()
